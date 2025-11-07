7acd18bcf4fb8c6d2e8066230465c91838a1b2cc
ldcard0724 <lucas.cardenas1382@gmail.com>
2025-11-07 07:19:38 -0600

    CHP 2 Functions: L2 - Get Files commit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,28 +1,19 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    # join working directory and directory together safely
-    full_path = os.path.join(working_directory, directory)
-
-    # check if full_path is inside the working directory, immediately stop if not
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    # Check if target directory is within the working directory, error if not
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as is is outside the permitted working directory'
+    # Check if target is actually a directory, error if not
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
     try:
-        abs_working_path = os.path.abspath(working_directory)
-        abs_full_path = os.path.abspath(full_path)
-        if not abs_full_path.startswith(abs_working_path):
-            return "f'Error: Cannot list \"{directory}\" as it is outside the permitted working directory'"
-    except Exception:
-        return "Error!"
-    
-    # check to ensure directory variable is actually a directory, error if not
-    try:
-        if not os.path.isdir(directory):
-            return "f'Error: \"{directory}\" is not a directory'"
-    except Exception:
-        return "Error 2!"
-    
-    # build output string
-    output_string = []
-    for file in os.listdir(abs_full_path):
-        output_string.append(f"- {file}: file_size={os.path.getsize(os.path.join(abs_full_path, file))}, is_dir={os.path.isdir(os.path.join(abs_full_path, file))}")
-    
-    return "\n".join(output_string)
+        output_string = []
+        for file in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, file)
+            output_string.append(f"- {file}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}")
+        return "\n".join(output_string)
+    except Exception as e:
+        return f"Error listing files: {e}"    
