83dd73f22a48e1c9dec1408a54b27465a9916e6d
ldcard0724 <lucas.cardenas1382@gmail.com>
2025-11-07 10:42:34 -0600

    CH2 Functions: L4 - Write File

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -35,3 +35,18 @@ def get_file_content(working_directory, file_path):
     with open(abs_file_path, "r") as f:
         file_content_string = f.read(MAX_CHARS)
     return file_content_string
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    # Check if target file is within the working directory, error if not
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: Unable to write file: {e}"
