122c3183dcebde94a14dcd53335b0d05aed70078
ldcard0724 <lucas.cardenas1382@gmail.com>
2025-11-07 08:19:35 -0600

    CHP 2 Functions: L3 - Get File Content commit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from config import MAX_CHARS
 
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
@@ -16,4 +17,21 @@ def get_files_info(working_directory, directory="."):
             output_string.append(f"- {file}: file_size={os.path.getsize(filepath)} bytes, is_dir={os.path.isdir(filepath)}")
         return "\n".join(output_string)
     except Exception as e:
-        return f"Error listing files: {e}"    
+        return f"Error listing files: {e}"
+    
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # Check if target file is within the working directory, error if not
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    # Check if target file is actually a file, error if not
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    # Read the file and return its contents as a string
+    with open(abs_file_path, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+    return file_content_string
